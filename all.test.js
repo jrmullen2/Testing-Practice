9c6088ba4ba2786d6a24cf06a7e0141b08bbe9c2
Jackson Mullen <jrmullen2@icloud.com>
2023-10-15 14:19:45 -0700

    Create additional test for analyzeArray

--- all.test.js
+++ all.test.js
@@ -21,8 +21,12 @@ test("Takes a string and a shift factor and returns it with each character “sh
     expect(caesarCipher("!@#$%^&*()", 10)).toBe("+J-./h0423");
 })
 test("Takes an array of numbers and returns an object with the following properties: average, min, max, and length.", () => {
-    expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).average).toBe(5.5);
+    expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).avg).toBe(5.5);
     expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).min).toBe(1);
     expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).max).toBe(10);
-    expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).length).toBe(10);
+    expect(analyzeArray([1,2,3,4,5,6,7,8,9,10]).lth).toBe(10);
+    expect(analyzeArray([2,4,6,8,10]).avg).toBe(6);
+    expect(analyzeArray([2,4,6,8,10]).min).toBe(2);
+    expect(analyzeArray([2,4,6,8,10]).max).toBe(10);
+    expect(analyzeArray([2,4,6,8,10]).lth).toBe(5);
 })
